dad356bc756280f8d8172a40b2684225a29db844
srinadhchekuri <cheku@172.29.0.5>
2019-01-26 15:04:06 -0500

    changed the name of the class from TextFile to ReadingTextFile

---
+++ prismITCorp/Didb/com/prismit/Multithreading/srinadh/ReadingTextFile.java
@@ -0,0 +1,18 @@
+package com.prismit.Multithreading.srinadh;
+import java.util.Scanner;
+import java.io.*; 
+
+public class ReadingTextFile {
+	public static void main(String[] args) throws Exception{ 
+		  // We need to provide file path as the parameter: 
+		  // double backquote is to avoid compiler interpret words 
+		  // like \test as \t (ie. as a escape sequence) 
+		  File file = new File("C:\\Users\\cheku\\Desktop\\file\\text.txt"); 
+		  
+		  BufferedReader br = new BufferedReader(new FileReader(file)); 
+		  
+		  String st; 
+		  while ((st = br.readLine()) != null) 
+		    System.out.println(st); 
+		  } 
+		} 
